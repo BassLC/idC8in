ea294dc3b7f2469b0c9d27fe2b1d49588a1fa7e9
BassLC <bernardolimaconde@gmail.com>
2016-12-17 22:10:39 +0000

    Printing opcodes. Almost done.

--- cpu.cpp
+++ cpu.cpp
@@ -1,11 +1,12 @@
+#include <bitset> //DEBUG: only for printing binary opcodes!
 #include <iostream>
 #include <string>
 #include <fstream>
-#include "SDL2/SDL.h"
 #include <array>
+#include "SDL2/SDL.h"
+
 
 class Chip8 {
-
 public:
   Chip8();
   ~Chip8();
@@ -13,7 +14,7 @@ public:
   void cycle();
   void decrease_timers();
   void decode_opcode();
-  
+
 private:
   std::array<uint8_t, 4096> memory;
   uint16_t opcode;
@@ -29,7 +30,6 @@ private:
   const int WINDOW_WIDTH = 32;
   SDL_Window* window = nullptr;
   SDL_Renderer* renderer = nullptr;
-    
 };
 
 
@@ -83,7 +83,7 @@ Chip8::Chip8(){
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
   };
 
-  for ( std::size_t i = 0; i < 80; ++i ) {memory[i] = fontset[i];}
+  for ( std::size_t i = 0; i < 80; ++i ) { memory[i] = fontset[i]; }
 }
 
 
@@ -115,45 +115,187 @@ bool Chip8::load_rom(const char *ROM_location) {
 
 
 void Chip8::cycle() {
+  if ( timer_sound > 0 ) {
+    std::cout << beep; //Using terminal specific interactions (BURRFIX)
+  }
+
   if ( timer_delay == 0 ) {
-    decode_opcode();
+    for ( auto i = 0x200; i<0x300; ++i ) { program_counter = i; decode_opcode(); }
   }
   decrease_timers();
+  
 }
 
 
 void Chip8::decode_opcode() {
   //Fetch opcode
   opcode = memory[program_counter] << 8 | memory[program_counter + 1]; 
-
+  //std::cout << std::hex << (opcode) << '\n';
+  
   switch ( opcode & 0xF000 ) {
+    
+  case 0x0000:
+    switch ( opcode & 0x0FFF) {
 
-  case 0x00E0: //Opcode to clear screen
-    std::cout << "CLR\n";
-    break;
+    case 0xE0:
+      std::cout << "CLR\n";
+      break;
+    case 0xEE:
+      std::cout << "RET\n";
+      break;
+    }
 
-  case 0x00EE: //Returns subroutine
-    std::cout << "RETSUB\n";
+  case 0x1000:
+    std::cout << "JMP1\n";
     break;
     
-  case 0x1000:
-    program_counter = memory[opcode & 0b1111];
-    std::cout << "JMP\n";
+  case 0x2000:
+    std::cout << "CALL2\n";
     break;
+
+  case 0x3000:
+    std::cout << "EQUAL REG BYTE\n";
+
+  case 0x4000:
+    std::cout << "NOT EQUAL REG4 BYTE\n";
+    break;
+    
+  case 0x5000:
+    std::cout << "EQUAL REG1 REG2\n";
+    break;
+
+  case 0x6000:
+    std::cout << "LOAD REG BYTE\n";
+    break;
+    
+  case 0x7000:
+    std::cout << "ADD REG BYTE\n";
+    break;
+
+  case 0x8000:
+
+    switch ( opcode & 0b1111 ) {
+    case 0:
+      std::cout << "LOAD REG1 REG2\n";
+      break;
+      
+    case 1:
+      std::cout << "OR REG1 REG2\n";
+      break;
+
+    case 2:
+      std::cout << "AND REG1 REG2\n";
+      break;
+      
+    case 3:
+      std::cout << "XOR REG1 REG2\n";
+      break;
+
+    case 4:
+      std::cout << "ADD REG1 REG2\n";
+      break;
+
+    case 5:
+      std::cout << "SUB REG1 REG2\n";
+      break;
+
+    case 6:
+      std::cout << "SHR REG1 REG2\n";
+      break;
+
+    case 7:
+      std::cout << "SUBN REG1 REG2\n";
+      break;
+
+    case 0xE:
+      std::cout << "SHL REG1 REG2\n";
+      break;
+    }
+
+  case 0x9000:
+    std::cout << "NOT EQUAL REG1 REG2 SKIP\n";
+    break;
+
+  case 0xA000:
+    std::cout << "SET INDEX BYTE\n";
+    break;
+
+  case 0xB000:
+    std::cout << "JMP ADDR+REG[0]\n";
+    break;
+    
+  case 0xC000:
+    std::cout << "SET REG1 (RANDOM AND BYTE)";
+    break;
+
+  case 0xD000:
+    std::cout << "DRW???\n";
+    break;
+
+  case 0xE000:
+    if ( (opcode & 0xFF) == 0x9E ) {
+      std::cout << "SKIP IF REG[1] PRESSED\n"; break;
+    } else {
+      std::cout << "SKIP IF REG[1] NOT PRESSED\n"; break;
+    }
+
+  case 0xF000:
+    switch ( opcode & 0xFF ) {
+     
+    case 0x7:
+      std::cout << "SET REG1 TIMER_DELAY\n";
+      break;
+
+    case 0x0A:
+      std::cout << "LOAD KEY VX\n";
+      break;
+
+    case 0x15:
+      std::cout << "SET TIMER_DELAY REG1\n";
+      break;
+
+    case 0x18:
+      std::cout << "SET TIMER_SOUND REG1\n";
+      break;
+
+    case 0x1E:
+      std::cout << "ADD I I+REG1\n";
+      break;
+
+    case 0x29:
+      std::cout << "LOAD F VX???\n";
+      break;
+
+    case 0x33:
+      std::cout << "LOAD B VX???\n";
+      break;
+
+    case 0x55:
+      std::cout << "LOAD I VX???\n";
+      break;
+
+    case 0x65:
+      std::cout << "LOAD VX I ???\n";
+      break;
+
+    default:
+      std::cout << "DEFAULT: " << std::hex << (opcode & 255);
+      break;
+    }
     
   default:
-    std::cout << "Could not read OPCODE: " << std::hex << opcode << ", "<< program_counter << std::endl;
+    std::cout << "Could not read OPCODE: " << std::hex << opcode << ", "<< std::dec << program_counter << std::endl;
     break;
   }
-  
+    
 }
-
 
 
 void Chip8::decrease_timers() {
-  if (timer_delay > 0) --timer_delay;
-  if (timer_sound > 0) --timer_sound;
+  if ( timer_delay > 0 ) --timer_delay;
+  if ( timer_sound > 0 ) --timer_sound;
 }
+
 
 int main(int argc, char *argv[]) {
   if ( argc != 2 ) {
--- makefile
+++ makefile
@@ -1,17 +1,17 @@
 #OBJS specifies which files to compile as part of the project
-OBJS =main
+OBJS=main
 
 #CC specifies which compiler we're using
-CC = clang++
+CC=clang++
 
 #C++ version
-VV= --std=c++14
+VV=--std=c++14
 
 # WARNINGS specifies all the warnings
-WARNINGS = -Wall -Wextra -Wpedantic
+WARNINGS=-Wall -Wextra -Wpedantic
 
 #LINKER_FLAGS specifies the libraries we're linking against
-LINKER_FLAGS = -lSDL2 -o
+LINKER_FLAGS=-lSDL2 -o
 
 #This is the target that compiles our executable
 all : $(OBJS)
